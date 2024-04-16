a48533754cdd75e0dc966d937639c33e967cc467
Tim Ludwig <timcat.tl@gmail.com>
2024-04-16 19:23:54 +0200

    repl for arithmetic and boolean expressions

---
+++ arith/arith_ast.py
@@ -0,0 +1,146 @@
+class Expression:
+    def __init__(self):
+        pass
+
+class NumberExpression(Expression):
+    def __init__(self, e):
+        self.e = e
+    
+    def eval(self):
+        return float(self.e)
+
+class BoolLiteralExpression(Expression):
+    def __init__(self, e):
+        self.e = e
+    
+    def eval(self):
+        return bool(self.e)
+
+class ParenExpression(Expression):
+    def __init__(self, e):
+        self.e = e
+    
+    def eval(self):
+        return self.e.eval()
+
+class PlusExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() + self.e2.eval()
+    
+class MinusExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() - self.e2.eval()
+    
+class TimesExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() * self.e2.eval()
+    
+class DivideExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() / self.e2.eval()
+    
+class LessExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() < self.e2.eval()
+    
+class GreaterExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() > self.e2.eval()
+    
+class LessEqualExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() <= self.e2.eval()
+    
+class GreaterEqualExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() >= self.e2.eval()
+    
+class EqualExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() == self.e2.eval()
+    
+class NotEqualExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() != self.e2.eval()
+    
+class NotExpression(Expression):
+    def __init__(self, e):
+        self.e = e
+    
+    def eval(self):
+        return not self.e().eval()
+    
+class AndExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() and self.e2.eval()
+    
+class OrExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        return self.e1.eval() or self.e2.eval()
+    
+class NandExpression(Expression):
+    def __init__(self, e1, e2):
+        self = NotExpression(AndExpression(e1, e2))
+    
+class NorExpression(Expression):
+    def __init__(self, e1, e2):
+        self = NotExpression(OrExpression(e1, e2))
+    
+class ImpExpression(Expression):
+    def __init__(self, e1, e2):
+        self.e1 = e1
+        self.e2 = e2
+    
+    def eval(self):
+        v1 = self.e1.eval()
+        v2 = self.e2.eval()
+        return v2 if v1 else 1
+    
---
+++ arith/arith_lexer.py
@@ -0,0 +1,38 @@
+from ply.lex import lex
+import re
+
+t_ignore = ' \t\n'
+
+tokens = [
+    'NUMBER',
+    'BOOL_LIT',
+    'PLUS', 'MINUS', 'TIMES', 'DIVIDE',
+    'LPAREN', 'RPAREN',
+    'LT', 'GT', 'LE', 'GE',
+    'EQ', 'NEQ',
+    'NOT', 'AND', 'OR', 'NAND', 'NOR', 'XOR', 'IMP'
+]
+
+t_NUMBER='\d+(\.\d+)?'
+t_BOOL_LIT='true|false'
+t_PLUS='\+'
+t_MINUS='-'
+t_TIMES='\*'
+t_DIVIDE='/'
+t_LPAREN='\('
+t_RPAREN='\)'
+t_LT='<'
+t_GT='>'
+t_LE='<='
+t_GE='>='
+t_EQ='=|eq'
+t_NEQ='!=|neq'
+t_NOT='not'
+t_AND='and'
+t_OR='or'
+t_NAND='nand'
+t_NOR='nor'
+t_XOR='xor'
+t_IMP='imp'
+
+lexer = lex(reflags=re.IGNORECASE)
