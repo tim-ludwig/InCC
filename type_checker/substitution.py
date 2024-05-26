85d7bb098e03c1fc727879cd4cfed5ca5df2b697
Tim Ludwig <timcat.tl@gmail.com>
2024-05-26 19:52:48 +0200

    type substitutions

---
+++ type_checker/substitution.py
@@ -0,0 +1,45 @@
+from dataclasses import dataclass
+
+from environment import Environment, Value
+from type_checker.types import TypeVar, Type, TypeFunc, PolyType
+
+
+@dataclass
+class Substitution:
+    subst: dict[str, Type]
+
+    def apply(self, t: Type):
+        match t:
+            case TypeVar(name):
+                return self.subst[name] if name in self.subst else t
+
+            case TypeFunc(name, args):
+                return TypeFunc(name, [self.apply(arg) for arg in args])
+
+            case PolyType(bound_var, s):
+                return PolyType(bound_var, Substitution({var: rep for var, rep in self.subst.items() if var != bound_var.name}).apply(s))
+
+    def apply_to_env(self, env: Environment):
+        env.vars = {name: Value(v.value, self.apply(v.type)) for name, v in env.vars.items()}
+
+        if env.parent:
+            self.apply_to_env(env.parent)
+
+    def compose(self, other):
+        subst = dict()
+
+        subst.update(self.subst)
+        subst.update({var: self.apply(rep) for var, rep in other.subst.items()})
+
+        return Substitution(subst)
+
+    def __call__(self, val):
+        match val:
+            case Type():
+                return self.apply(val)
+
+            case Substitution():
+                return self.compose(val)
+
+            case Environment():
+                return self.apply_to_env(val)
