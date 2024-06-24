fa07e779655c533e90f94eebc2c48b208d0f999d
Tim Ludwig <timcat.tl@gmail.com>
2024-06-24 17:32:34 +0200

    refactor lambda and function parser

--- parser/functions.py
+++ parser/functions.py
@@ -13,41 +13,6 @@ def p_ident_list(p):
         p[0] = [p[1], *p[3]]
 
 
-def p_expression_lambda_(p):
-    """
-    expression : lambda_expression
-    """
-    p[0] = p[1]
-
-
-def p_expression_lambda(p):
-    """
-    lambda_expression : BACKSLASH RIGHT_ARROW expression
-                      | BACKSLASH ident_list RIGHT_ARROW expression
-    """
-    if len(p) == 5:
-        p[0] = LambdaExpression(p[2], p[4])
-    else:
-        p[0] = LambdaExpression([], p[3])
-
-
-def p_expression_lambda_rest_arg(p):
-    """
-    lambda_expression : BACKSLASH ident_list DOT DOT DOT RIGHT_ARROW expression
-    """
-    p[0] = LambdaExpression(p[2], p[7], True)
-
-
-def p_expression_function(p):
-    """
-    expression : FUN IDENT lambda_expression
-    """
-    #    fun f x -> body
-    # => f := local f := x -> body in f
-    f_assign_lmbd = AssignExpression(p[2], p[3])
-    p[0] = AssignExpression(p[2], LocalExpression(f_assign_lmbd, VariableExpression(p[2])))
-
-
 def p_expr_list(p):
     """
     expr_list : expression
@@ -59,10 +24,46 @@ def p_expr_list(p):
         p[0] = [p[1], *p[3]]
 
 
+def p_expression_lambda(p):
+    """
+    expression : BACKSLASH RIGHT_ARROW expression
+               | BACKSLASH ident_list RIGHT_ARROW expression
+               | BACKSLASH ident_list DOT DOT DOT RIGHT_ARROW expression
+    """
+    match len(p):
+        case 4:
+            p[0] = LambdaExpression([], p[3])
+        case 5:
+            p[0] = LambdaExpression(p[2], p[4])
+        case 8:
+            p[0] = LambdaExpression(p[2], p[7], True)
+
+
+def p_expression_function(p):
+    """
+    expression : FUN IDENT RIGHT_ARROW expression
+               | FUN IDENT ident_list RIGHT_ARROW expression
+               | FUN IDENT ident_list DOT DOT DOT RIGHT_ARROW expression
+    """
+    #    fun f x -> body
+    # => f := local f := \x -> body in f
+    match len(p):
+        case 5:
+            lmbd_expr = LambdaExpression([], p[4])
+        case 6:
+            lmbd_expr = LambdaExpression(p[3], p[5])
+        case 9:
+            lmbd_expr = LambdaExpression(p[3], p[8], True)
+
+    local_expr = LocalExpression(AssignExpression(p[2], lmbd_expr), VariableExpression(p[2]))
+
+    p[0] = AssignExpression(p[2], local_expr)
+
+
 def p_expression_call(p):
     """
-    expression : expression LPAREN expr_list RPAREN
-               | expression LPAREN RPAREN
+    expression : expression LPAREN RPAREN
+               | expression LPAREN expr_list RPAREN
     """
     if len(p) == 4:
         p[0] = CallExpression(p[1], [])
